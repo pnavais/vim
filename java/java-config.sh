c9dd1a0178887cee0da8a61ce21bc32a3f86de7d
Pablo Navais <pnavais@gmail.com>
2019-11-21 09:35:23 +0100

    Fixed root privileges for sdkman installation

--- java/java-config.sh
+++ java/java-config.sh
@@ -13,6 +13,9 @@
 SCRIPT_DIR=$(cd "$(dirname ${BASH_SOURCE[0]})"; pwd);
 BASH_MAIN="$SCRIPT_DIR/../bash";
 
+SUDO="sudo"
+
+
 # Functions
 ###########
 
@@ -23,10 +26,25 @@ source $BASH_MAIN/bash_deps.sh
 
 showSection "Performing Java Customization";
 
+if [ "$EUID" -ne 0 ]; then
+	CAN_I_RUN_SUDO=$(sudo -n uptime 2>&1|grep "load"|wc -l)
+	if [ ${CAN_I_RUN_SUDO} -eq 0 ]; then
+		# Ask for the administrator password upfront
+		warn "This script needs elevated permissions to execute.\nPlease provide super-user credentials to continue.\n"
+		sudo -v
+	fi
+
+	# Keep-alive: update existing `sudo` time stamp until finished
+	while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+else
+	SUDO=""
+fi
+
 # Installing JVM Tools
 showSubSection "Installing JVM tools";
 if [[ ! -e "/usr/local/sdkman" ]]; then
 	printf "$(pad "Installing $(ansi --green \"SDKMAN\!\")")";
-	export SDKMAN_DIR="/usr/local/sdkman" && curl -s "https://get.sdkman.io" | bash &>/dev/null
+	$SUDO bash -c "export SDKMAN_DIR=\"/usr/local/sdkman\" && curl -s \"https://get.sdkman.io\" | bash &>/dev/null";
+	$SUDO chown -R $USER:$GID /usr/local/sdkman
 	showResultOrExit;
 fi
